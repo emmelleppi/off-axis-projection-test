8797d17fbe975dabe514a29bd98d807d521ae354
emmelleppi <mlp.perego@gmail.com>
2020-08-05 16:24:42 +0200

    New wireframe test

--- src/index.js
+++ src/index.js
@@ -156,21 +156,29 @@ function DepthCube({ scene, camera }) {
             </group>
           </Suspense>
         </Physics>
-        <group position={[0, 0, 0.5]} >
+        {/* <group position={[0, 0, 0.5]} >
           <group scale={[1,1,2]} position={[0,0,0.5]} >
             <MyBox speed={(1 + Math.random()) * 0.0001} />
             <MyBox speed={(1 + Math.random()) * 0.0001} rotation={[0,0,Math.PI/2]} />
             <MyBox speed={(1 + Math.random()) * 0.0001} />
             <MyBox speed={(1 + Math.random()) * 0.0001} rotation={[0,0,Math.PI/2]} />
           </group>
-        </group>
-        <Box position={[0, 0, 0.49]} args={[1, 1, 1]}>
+        </group> */}
+        <Box position={[0, 0, 0.5]} args={[1, 1, 1]}>
           <meshBasicMaterial side={THREE.BackSide} color={0x0000ff} attachArray="material" />
           <meshBasicMaterial side={THREE.BackSide} color={0x0000ff} attachArray="material" />
           <meshBasicMaterial side={THREE.BackSide} color={0x0000ff} attachArray="material" />
           <meshBasicMaterial side={THREE.BackSide} color={0x0000ff} attachArray="material" />
           <meshBasicMaterial side={THREE.BackSide} color={0x0000ff} transparent opacity={0} attachArray="material" />
           <meshBasicMaterial side={THREE.BackSide} color={0x0000ff} attachArray="material" />
+        </Box>
+        <Box position={[0, 0, 0.5]} args={[1, 1, 1, 8,16,16]}>
+          <meshBasicMaterial wireframe attachArray="material" />
+          <meshBasicMaterial wireframe attachArray="material" />
+          <meshBasicMaterial wireframe attachArray="material" />
+          <meshBasicMaterial wireframe attachArray="material" />
+          <meshBasicMaterial wireframe transparent opacity={0} attachArray="material" />
+          <meshBasicMaterial wireframe attachArray="material" />
         </Box>
         <ambientLight intensity={1} />
       </group>
